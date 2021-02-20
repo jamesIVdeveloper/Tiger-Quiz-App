a3c8ad59fec203e823d3f935bf619ae847dd5f2c
James <68920695+jamesIVdeveloper@users.noreply.github.com>
2021-02-20 15:22:34 -0600

    Update app.js

--- app.js
+++ app.js
@@ -32,25 +32,6 @@ const store = {
   score: 0,
 };
 
-
-/**
- * 
- * Technical requirements:
- * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
- * See your course material and access support for more details.
- *
- * NO additional HTML elements should be added to the index.html file.
- *
- * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
- *
- * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
- * 
- */
-
-/********** TEMPLATE GENERATION FUNCTIONS **********/
-
-// These functions return HTML templates
 function generateBeginQuiz() {
   return `
     <div class="begin-quiz">
@@ -145,13 +126,6 @@ function generateFeedback(answerStatus) {
   return html;
 }
 
-
-
-
-/********** RENDER FUNCTION(S) **********/
-
-// This function conditionally replaces the contents of the <main> tag based on the state of the store
-
 function render() {
   let html = '';
   if (store.quizStarted === false) {
@@ -167,11 +141,6 @@ function render() {
     $('main').html(generateResults());
   }
 }
-
-
-/********** EVENT HANDLER FUNCTIONS **********/
-
-// These functions handle events (submit, click, etc)
 
 function handleBeginClick() {
   $('main').on('click', '#begin', function(event) {
